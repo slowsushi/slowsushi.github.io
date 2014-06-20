d3794f6635b9fc02175514b8f03b1c0a65ffdf4c
slowsushi <wuytsky@gmail.com>
2014-06-20 15:42:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function changeColor(){
   var colors = ["#CCCCCC","#333333","#990099","#71C671","#F4A460","#FFFF00","#00FF00","#104E8B","#8B4789","#B0171F"];                
   var rand = Math.floor(Math.random()*colors.length);           
   $('body').css("background-color", colors[rand]);
-  rand = Math.floor(Math.random()*colors.length);
+  $ rand = Math.floor(Math.random()*colors.length);
   $('h1').css("color", colors[rand]);
    setInterval(changeColor,500);
 
