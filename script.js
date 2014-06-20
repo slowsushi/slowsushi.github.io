3503a6c284ba87d68f7601d456ca7ce5d1203849
slowsushi <wuytsky@gmail.com>
2014-06-20 17:54:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(document).ready(setInterval(function(){
-  var colors = ["#CCCCCC","#333333","#990099","#71C671","#F4A460","#FFFF00","#00FF00","#104E8B","#8B4789","#B0171F"];                
-  var rand = Math.floor(Math.random()*colors.length);           
-  $('body').css("background-color", colors[rand]);
-  var rand = Math.floor(Math.random()*colors.length);
-  $('h1').css("color", colors[rand]);},500)
-  );
+$(document).ready(setInterval(function() {
+    var colors = ["#CCCCCC", "#333333", "#990099", "#71C671", "#F4A460", "#FFFF00", "#00FF00", "#104E8B", "#8B4789", "#B0171F"];
+    var rand = Math.floor(Math.random() * colors.length);
+    $('body').css("background-color", colors[rand]);
+    var randTwo = Math.floor(Math.random() * colors.length);
+    $('h1').css("color", colors[randTwo]);
+}, 500));
