34b9539946fd165f3f502d735e6806080599bc86
slowsushi <wuytsky@gmail.com>
2014-06-20 14:43:04 +0200

    Update change.js

--- script/change.js
+++ script/change.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){
-  var colors = ["#CCCCCC","#333333","#990099"];                
+$(document).ready(function changeClor(){
+  var colors = ["#CCCCCC","#333333","#990099","#71C671","#F4A460","#FFFF00","#00FF00","#104E8B","#8B4789","#B0171F"];                
   var rand = Math.floor(Math.random()*colors.length);           
   $('body').css("background-color", colors[rand]);
   rand = Math.floor(Math.random()*colors.length);
   $('h1').css("color", colors[rand]);
+   setInterval(changeColor,500);
+
 });
