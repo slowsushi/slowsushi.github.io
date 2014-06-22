ed70710036c4ff6039d5765386f0057e5d15c49a
slowsushi <wuytsky@gmail.com>
2014-06-22 12:45:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ $(document).ready(setInterval(function() {
     $('body').css("background-color", colors[rand]);
     
     var randTwo = Math.floor(Math.random() * colors.length);
-    $('div').css("color", colors[randTwo]);
+    $('h1').css("color", colors[randTwo]);
     
     var marginL = ["5","30","50","100","200","250","230","290","230","240","250","260","270","280","290","300","310","320","440","450","460","470","480","490","500","510","520","530","540","550","560","570","580"];
     var randLMargin = Math.floor(Math.random()*marginL.length);
-    $('h1').css("left",marginL[randLMargin]);
+    $('div').css("left",marginL[randLMargin]);
     
     var marginTop = ["5","30","50","100","200","250","230","290","230","240","250","260","270","280","290","300","310","320","440","450","460","470","480","490","500","510","520","530","540","550","560","570","580"];
     var randTMargin = Math.floor(Math.random()*marginTop.length);
