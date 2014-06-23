f0cddad5c4be85429636cdd61006b1310e2d42d7
slowsushi <wuytsky@gmail.com>
2014-06-23 12:59:59 +0000

    script

--- script.js
+++ script.js
@@ -10,4 +10,12 @@ $(document).ready(setInterval(function() {
     var randTwo = Math.floor(Math.random() * colors.length);
     $('h1').css("color", colors[randTwo]);
     
+    var marginL = ["5px","30px","50px","100px","200px","250px","230px","290px","230px","240px","250px","260px","270px","280px","290px","300px","310px","320px","440px","450px","460px","470px","480px","490px","500px","510px","520px","530px","540px","550px","560px","570px","580px"];
+    var randLMargin = Math.floor(Math.random()*marginL.length);
+    $('div').css("margin-left",marginL[randLMargin]);
+    
+    var marginTop = ["5px","10px","15px","20px","25px","30px","35px","50px","100px","200px","250px","230px","290px","230px","240px","250px","260px","270px","280px","290px","300px","310px","320px","440px","450px","460px","470px","480px","490px","500px","510px","520px","530px","540px","550px","560px","570px","580px"];
+    var randTMargin = Math.floor(Math.random()*marginTop.length);
+    $('div').css("margin-top",marginTop[randTMargin]);
+    
 }, 1000));
